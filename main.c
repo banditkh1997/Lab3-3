5e8fd10a89519c8a403ec67cda0d7d881076a9bc
BanditKhawyen <banditkh1997@hotmail.com>
2020-08-17 12:52:20 +0000

    2

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void) {
-  printf("Hello World\n");
+int main(void)
+{
+  float a, b,total;
+  printf("Enter A : ");
+  scanf("%f", &a);
+  printf("Enter B : ");
+  scanf("%f", &b);
+  total=a*b;
+  printf("Total = %.2f\n",total);
   return 0;
 }
